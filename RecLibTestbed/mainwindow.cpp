3fc93b25f380359c1970700514a8d8d7b782695b
h0n3ycl0ud <jasonclevandoski@gmail.com>
2017-02-21 02:33:07 -0500

    Update mainwindow.cpp

--- RecLibTestbed/mainwindow.cpp
+++ RecLibTestbed/mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QMessageBox b;
+    b.setText("Hello, world!");
+    b.show();
 }
 
 MainWindow::~MainWindow()
